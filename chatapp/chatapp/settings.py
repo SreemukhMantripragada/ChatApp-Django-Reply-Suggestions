9abe79694c4c49f2ce25c41ce2423012a8fac481
Sree Mantri <sreemntr@gmail.com>
2021-07-30 13:59:34 +0530

    _

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -42,7 +42,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)q*k!p=5h&)g!ulsz9%0hu6kioilr62h_hxz)lf-*w@8pv5*tl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
